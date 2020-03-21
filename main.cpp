89ca62514dc6ef59c5a0ad4e61330391d196726e
Tomato <weijun.mai@hotmail.com>
2020-03-21 17:21:44 +0800

    create basic files

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
+
+int main() {
+
+}
